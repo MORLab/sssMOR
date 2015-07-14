52a04fd8d4660106f914f92d26477cf3cc0d7116
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-07-14 17:46:13 +0200

    SPYSYS: changed to allow for multiple inputs

--- software/extras/plotting/niceplots/spysys.m
+++ software/extras/plotting/niceplots/spysys.m
@@ -1,20 +1,20 @@
-function fh = spysys(A,E)
+function fh = spysys(varargin)
 
-% spysys(A,E)
+% spysys(E,A)
 % spysys(sys)
 
 %   parse input
-if strcmp(class(A),'sss')
-    sys = A;
+if nargin == 2
+    E = varagin{1};
+    A = varargin{2};
+else
+    sys = varargin{1};
     A = sys.a;
     E = sys.e;
 end
 
 fh = nicefigure('Sparsity pattern of system matrices');
 
-% subplot(1,2,1); spy(A,'Color',TUM_Blau); title('A');
-% subplot(1,2,2); spy(E,'Color',TUM_Blau); title('E');
-
-subplot(1,2,1); spy(A); title('A');
-subplot(1,2,2); spy(E); title('E');
+subplot(1,2,1); spy(E); title('E');
+subplot(1,2,2); spy(A); title('A');
 
