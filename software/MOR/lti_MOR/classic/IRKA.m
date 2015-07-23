6596e84bb75607478ebf5232a99d91bbf49c8e88
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-07-23 14:55:55 +0200

    IRKA
    - header
    - n Inputs/Outputs
    - Options structure
    - Stopping Criteria

--- software/MOR/lti_MOR/classic/IRKA.m
+++ software/MOR/lti_MOR/classic/IRKA.m
@@ -1,51 +1,73 @@
-function [sysr, V, W, s0, s0_traj] = IRKA(sys, s0, maxiter, epsilon,opts) 
-% IRKA Algorithm for Model Order Reduction by Krylov Subspace Methods
+function [sysr, V, W, s0, s0_traj] = IRKA(sys, s0, Opts) 
+% IRKA - Iterative Rational Krylov Algorithm
 % ------------------------------------------------------------------
-% [sysr, V, W, s0] = IRKA(sys, [s0;q], maxiter, epsilon)
-% Inputs:       * sys: an sss-object containing the LTI system
-%               * s0: initial expansion point
-%               * maxiter: maximum number of iterations
-%               * epsilon: maximum step size for convergence
-%               * opts: choose between original IRKA ('orig') and a 
-%                       stability preserving version ('stab')
-% Outputs:      * sysr: reduced system
-%               * V, W: Projection matrices spanning Krylov subspaces
-%               * s0: vector containing optimal expansion points
-%               * s0_traj: complete history of s0 over iterations
+% [sysr, V, W, s0, s0_traj] = IRKA(sys, s0, Opts)
+% Inputs:       * sys: full oder model (sss)
+%               * s0:  vector of initial shifts
+%               * Opts (opt.) structure with execution parameters
+% Outputs:      * sysr: reduced order model (sss)
+%               * V,W:  resulting projection matrices
+%               * s0:   final choice of shifts
+%               * s0_traj: trajectory of all shifst for all iterations
 % ------------------------------------------------------------------
-% For further explanation of the notation of s0, please refer to
-% <a href="matlab:help RK">help RK</a>.
+% USAGE:  This function executes the Iterative Rational Krylov
+% Algorithm (IRKA) as proposed by Gugergin and Beattie in [1].
+%
+% The IRKA iteration is conducted to search for an optimal set of 
+% shifts in Krylov subspace-based model reduction. If IRKA converges,
+% then the reduced model is known to be a local optimum with respect
+% to the H2 norm of the error.
+%
+% See also ARNOLDI, RK.
+%
 % ------------------------------------------------------------------
-% This file is part of the MORLAB_GUI, a Model Order Reduction and
-% System Analysis Toolbox developed at the
-% Institute of Automatic Control, Technische Universitaet Muenchen
+% REFERENCES:
+% [1] Gugercin (2008), H2 model reduction for large-scale linear
+%     dynamical systems
+% ------------------------------------------------------------------
+% This file is part of MORLab, a Sparse State Space, Model Order
+% Reduction and System Analysis Toolbox developed at the Institute 
+% of Automatic Control, Technische Universitaet Muenchen.
 % For updates and further information please visit www.rt.mw.tum.de
+% For any suggestions, submission and/or bug reports, mail us at
+%                   -> MORLab@rt.mw.tum.de <-
 % ------------------------------------------------------------------
-% Authors:      Alessandro Castagnotto (a.castagnotto@tum.de),
-%               Heiko Panzer (heiko@mytum.de)
-% Last Change:  24 Feb 2015
+% Authors:      Heiko Panzer, Alessandro Castagnotto
+%               (a.castagnotto@tum.de)
+% Last Change:  23 Jul 2015
+% Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
 % ------------------------------------------------------------------
+%% Parse input and load default parameters
+    % default values
+    
+%% Parse the inputs
+%   Default execution parameters
+Def.maxiter = 50; 
+Def.epsilon = 1e-3; 
+Def.type = ''; %'stab', 'newton','restarted'
+Def.verb = 0; % text output durint iteration?
+Def.stopCrit = 'combAny'; %'s0', 'sysr', 'combAll', 'combAny'
 
-if ~exist('maxiter', 'var') || isempty(maxiter)
-    maxiter=50;
+% create the options structure
+if ~exist('Opts','var') || isempty(Opts)
+    Opts = Def;
+else
+    Opts = parseOpts(Opts,Def);
 end
-if ~exist('epsilon', 'var') || isempty(epsilon)
-    epsilon=1e-3;
-elseif epsilon<=0 || ~isreal(epsilon)
+
+% Further check/transform the input
+if Opts.epsilon<=0 || ~isreal(Opts.epsilon)
     error('epsilon must be a real positive number.');
-end
-if ~exist('opts', 'var') || isempty(opts)
-    opts = 'stab';
 end
 
 s0 = s0_vect(s0);
-tic
 
-sysr=sss([],[],[]);
-
-s0_traj = zeros(maxiter+2, length(s0));
+% Initialize variables
+sysr = sss([],[],[]);
+s0_traj = zeros(Opts.maxiter+2, length(s0));
 s0_traj(1,:) = s0;
 
+%% IRKA iteration
 k=0;
 while true
     k=k+1;
@@ -59,51 +81,40 @@ while true
     s0(isnan(s0)) = 0;
     s0 = cplxpair(s0);
 
-    if strcmp(opts,'stab')
+    if strcmp(Opts.type,'stab')
         % mirror shifts with negative real part
         s0 = s0.*sign(real(s0));
     end
 
     s0_traj(k+1,:) = s0;
-%     plot([s0_old;s0], '-*'); hold on
     
-    es0 = norm((s0-s0_old)./s0, 1)/sysr.n;
-
-    e=inf;
-    if all(real(s0)>0)
-        warning off
-        if isreal(norm(sysr))
-            e=norm(sysr-sysr_old)/norm(sysr);
-        end
-        warning on
+    
+    [stop, stopCrit] = stoppingCriterion(s0,s0_old,sysr,sysr_old,Opts);
+    if Opts.verb
+        fprintf('IRKA step %03u - Convergence (%s): %s \n', ...
+            k, Opts.stopCrit, sprintf('% 3.1e', stopCrit));
     end
     
-    if sys.n>500
-        disp(['IRKA step ' num2str(k,'%03u') 9 ': Convergence '  num2str(es0, '%3.1e'), ', ' num2str(e, '%3.1e')]);
-    end
-    
-    if es0 < epsilon || k >= maxiter %  || e < epsilon
+    if stop || k>= Opts.maxiter
         s0 = s0_old; % function return value
         s0_traj = s0_traj(1:(k+1),:);
         break
-    end
+    end      
 end
 
-% assignin('base', 'IRKA_s0', s0_traj);
 
-if sys.n<=500
-    disp(['IRKA step ' num2str(k,'%03u') 9 ': Convergence '  num2str(es0, '%3.1e'), ', ' num2str(e, '%3.1e')]);
+if ~Opts.verb %display at least the last value
+    fprintf('IRKA step %03u - Convergence (%s): %s \n', ...
+            k, Opts.stopCrit, sprintf('% 3.1e', stopCrit));
 end
 
-disp(['IRKA exit. Elapsed time: ' num2str(toc) 's']);
-
-if k>=maxiter
+if k>=Opts.maxiter
     warning('IRKA:no_converged', ['IRKA has not converged after ' num2str(k) ' steps.']);
     return
 end
 
-end
 
+%------------------ AUXILIARY FUNCTIONS -------------------
 function s0=s0_vect(s0)
     % change two-row notation to vector notation
     if size(s0,1)==2
@@ -119,5 +130,37 @@ function s0=s0_vect(s0)
     if size(s0,1)>size(s0,2)
         s0=transpose(s0);
     end
+function [stop,stopCrit] = stoppingCriterion(s0,s0_old,sysr,sysr_old,Opts)
+%   Computes the stopping criterion(s) for IRKA
+%   It returns a scalar or a vector depending on the number of criteria
+%   chosen
+switch Opts.stopCrit
+    case 's0' %shift convergence
+        stopCrit = norm((s0-s0_old)./s0, 1)/sysr.n;
+        
+    case 'sysr' %reduced model convergence
+        stopCrit = inf; %initialize in case the reduced model is unstable
+        if all(real(eig(sysr))<0) && all(real(eig(sysr_old))<0)
+                stopCrit=norm(sysr-sysr_old)/norm(sysr);
+        end
+        stop = stopCrit <= Opts.epsilon;
+    case 'combAll'
+        stopCrit = norm((s0-s0_old)./s0, 1)/sysr.n;
+        stopCrit = [stopCrit, inf]; 
+        if all(real(eig(sysr))<0) && all(real(eig(sysr_old))<0)
+                stopCrit(2) = norm(sysr-sysr_old)/norm(sysr);
+        end
+        stop = all(stopCrit <= Opts.epsilon);
+    case 'combAny'
+        stopCrit = norm((s0-s0_old)./s0, 1)/sysr.n;
+        stopCrit = [stopCrit, inf]; 
+        if all(real(eig(sysr))<0) && all(real(eig(sysr_old))<0)
+                stopCrit(2) = norm(sysr-sysr_old)/norm(sysr);
+        end
+        stop = any(stopCrit <= Opts.epsilon);
+    otherwise
+        error('The stopping criterion selected is incorrect or not implemented')
 end
+
+
 
