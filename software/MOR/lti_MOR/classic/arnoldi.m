0b6dae036949b083361204084d11c48b39bab290
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-07-24 11:19:36 +0200

    Arnoldi: added code for analysis

--- software/MOR/lti_MOR/classic/arnoldi.m
+++ software/MOR/lti_MOR/classic/arnoldi.m
@@ -98,16 +98,20 @@ if ~isempty(k)
     s0 = [s0 s0c(1:2:end)];
 end
 
+%---
+eLSE = zeros(1,length(s0));
+eGS  = zeros(1,length(s0));
+
 % preallocate memory
 V=zeros(length(b),q);
 Ct=eye(1,q); %**
-if hermite, W = zeros(length(b),q); Bt = eye(1,q)';end
+if hermite, W = zeros(length(b),q); Bt = eye(1,q)'; W2 = zeros(size(W));end
 
 for jCol=1:length(s0)
     % new basis vector
     tempV=b; newlu=1; 
     Ct(jCol)=1; %**
-    if hermite, tempW = c'; Bt(jCol) = 1; end
+    if hermite, tempW = c'; Bt(jCol) = 1; tempW2 = c'; end ;
     if jCol>1
         if s0(jCol)==s0(jCol-1)
             tempV=V(:,jCol-1);
@@ -154,8 +158,12 @@ for jCol=1:length(s0)
         tempV(o,:) = U\(L\(S(:,p)\tempV)); %LU x(o,:) = S(:,p)\b 
 %         if hermite, tempW = (S(:,p)).'\(L.'\(U.'\(tempW(o,:)))); end %U'L'S(:,p) x = c'(o,:) 
         if hermite,
-            [Lt,Ut,pt,ot,St]=lu(sparse((A'-s0(jCol)*E')),'vector');
-            tempW(ot,:) = Ut\(Lt\(St(:,pt)\tempW));
+            %--
+            [Lt,Ut,pt,ot,St]=lu(sparse((A.'-s0(jCol)*E.')),'vector');
+            tempW2(ot,:) = Ut\(Lt\(St(:,pt)\tempW));
+            %--
+            tempW = (S(:,p)).'\(L.'\(U.'\(tempW(o,:)))); 
+            eLSE(jCol) = norm(tempW2-tempW);
         end %U'L'S(:,p) x = c'(o,:) 
     end 
 
@@ -163,7 +171,10 @@ for jCol=1:length(s0)
     if ~isreal(s0(jCol))
         V(:,jCol+length(s0c)/2)=imag(tempV); 
         tempV=real(tempV);
-        if hermite, W(:,jCol+length(s0c)/2)=imag(tempW);tempW=real(tempW);end
+        if hermite, W(:,jCol+length(s0c)/2)=imag(tempW);tempW=real(tempW);
+            %--
+            W2(:,jCol+length(s0c)/2)=imag(tempW2);tempW2=real(tempW2);
+        end
     end
 
     % orthogonalize vectors
@@ -175,6 +186,8 @@ for jCol=1:length(s0)
         h=IP(tempW,W(:,iCol));
         tempW=tempW-W(:,iCol)*h;
         Bt(jCol)=Bt(jCol)-h*Bt(iCol);
+        %--
+        h=IP(tempW2,W2(:,iCol));tempW2=tempW2-W2(:,iCol)*h;
       end
           
     end
@@ -187,8 +200,17 @@ for jCol=1:length(s0)
         h = sqrt(IP(tempW,tempW));
         W(:,jCol)=tempW/h;
         Bt(jCol) = Bt(jCol)/h;
+        %--
+        h = sqrt(IP(tempW2,tempW2)); W2(:,jCol)=tempW2/h; 
+        eGS(jCol) = norm(W-W2);
     end
    
+end
+if hermite, 
+    figure; semilogy(eLSE,'-b');hold on, semilogy(eGS,'-r'), 
+    title('Error by using same LU decomposition');
+    ylabel('2 norm of error');xlabel('arnoldi iteration');
+    legend('solution of the LSE','projection matrix after GS','location','NorthWest');
 end
 
 %orthogonalize columns from imaginary components
