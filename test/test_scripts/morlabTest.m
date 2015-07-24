e30a3d11a39b4f966d8029266e3f112e2df13976
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-07-24 11:16:32 +0200

    morlabTest - added test cases (complex numbers)

--- test/test_scripts/morlabTest.m
+++ test/test_scripts/morlabTest.m
@@ -13,16 +13,18 @@ sys = sss(A,B,C,D,E)
 bode(sys)
 
 %% Test RK and Arnoldi
-% clear, 
+clear, 
 clc
 
 load build
 sys = sss(A,B,C);
 
-s0 = -rand(1,20);
-tic,[sysr1i,V1i,W1i]    = RK(sys,s0);t1i = toc
-tic,[sysr1o,V1o,W1o]    = RK(sys,[],s0); t1o = toc
-tic,[sysr2,V2,W2]       = RK(sys,s0,s0); t2 = toc
+% s0 = rand(1,20);
+s0 = rand(1,10) + 1i*rand(1,10);
+s0 = [s0, conj(s0)];
+tic,[sysr1i,V1i,W1i]    = RK(sys,s0);t1i = toc;
+tic,[sysr1o,V1o,W1o]    = RK(sys,[],s0); t1o = toc;
+tic,[sysr2,V2,W2]       = RK(sys,s0,s0); t2 = toc;
 
 norm(V1i-V2)
 norm(W1o-W2)
@@ -30,16 +32,18 @@ norm(W1o-W2)
 sysr3 = sss(W1o'*sys.a*V1i,W1o'*sys.b, sys.c*V1i,sys.d,W1o'*sys.e*V1i);
 figure;bode(sys,'k');hold on, bode(sysr2,'r-'); bode(sysr3,'g--');
 
-norm(eig(sysr2)-eig(sysr3))
+norm(cplxpair(eig(sysr2))-cplxpair(eig(sysr3)))
+norm(sysr3-sysr2)/norm(sysr3)
 
 %%  Test Arnoldi (invsolve)
 clear, clc
 n = 1e2; density = 0.1;
-A = sprand(n,n,density); E = sprand(n,n,density); b = sprand(n,1,density);
+A = sprand(n,n,density)+ 1i*sprand(n,n,density); 
+E = sprand(n,n,density); b = sprand(n,1,density) + 1i*sprand(n,1,density);
 Id = speye(size(A));
 
 [L,U,p,o,S]=lu(A,'vector');
-[Lt,Ut,pt,ot,St] = lu(A','vector');
+[Lt,Ut,pt,ot,St] = lu(A.','vector');
     
 % Input Krylov
 fprintf('--- input Krylov ---');
@@ -85,33 +89,34 @@ fprintf('--- input Krylov ---');
 % Output Krylov
 fprintf('--- output Krylov ---');
     %1st direction
-    xCorr = A'\b;
+    xCorr = A.'\b;
     xCorr2(ot,:) = Ut\(Lt\(St(:,pt)\b));
     eLUt = norm(xCorr-xCorr2)
 
     %a) direct computation
     tic
-    xDirect = (S*Id(:,p))'\(L'\(U'\(Id(:,o)'*b)));
+    xDirect = (S*Id(:,p)).'\(L.'\(U.'\(Id(:,o)'*b)));
     tDirect = toc
     eDirect = norm(xCorr-xDirect)
     
     %c) New: a mixture of both a and b
     tic
-    xNew = (S(:,p))'\(L'\(U'\(b(o,:))));
+    xNew = (S(:,p)).'\(L.'\(U.'\(b(o,:))));
     tNew = toc
     eNew = norm(xCorr-xNew)
-
     
     %2nd direction
-    xCorr = A'\(E'*xCorr);
+    xCorr2(ot,:) = Ut\(Lt\(St(:,pt)\(E'*xCorr)));
+    xCorr = A.'\(E'*xCorr);
+    eLUt = norm(xCorr-xCorr2)
     
     %b) direct computation
-    xDirect = (S*Id(:,p))'\(L'\(U'\(Id(:,o)'*(E'*xDirect))));
+    xDirect = (S*Id(:,p)).'\(L.'\(U.'\(Id(:,o)'*(E'*xDirect))));
     eDirect = norm(xCorr-xDirect)
     
     %c) New: a mixture of both a and b
     temp = E'*xNew;
-    xNew = (S(:,p))'\(L'\(U'\(temp(o,:))));
+    xNew = (S(:,p)).'\(L.'\(U.'\(temp(o,:))));
     eNew = norm(xCorr-xNew)
     
 %%  Test IRKA
@@ -119,10 +124,10 @@ clear, clc
 
 load build
 sys = sss(A,B,C);
-s0 = -rand(1,10);
-Opts = struct('maxiter',100,'epsilon',1e-3,'stopCrit','combAny','verb',1);
+s0 = rand(1,40);
+Opts = struct('maxiter',100,'epsilon',1e-3,'stopCrit','combAll','verb',1);
 [sysr, V, W, s0, s0_traj] = IRKA(sys, s0, Opts);
-% analyze_MOR(sys,sysr);
+analyze_MOR(sys,sysr);
 %% Test CURE
 close all, clear, clc
 load fom; %beam, fom
