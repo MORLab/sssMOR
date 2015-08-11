558a5a9c2117ee7dff24548698a12f9a6bb0ebad
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-08-11 11:06:20 +0200

    GUI added

---
+++ GUI/isvalidvarname.m
@@ -0,0 +1,20 @@
+function isvalidvarname(hObject,varargin)
+% Überprüft den String des Übergebenen Objekts auf Gültigkeit des
+% Variablennamens und ob schon eine Variable mit diesem Namen im workspace
+% liegt
+% über varargin{1}=='NoWarning' kann warnung unterdrückt werden
+
+% letzte Änderung 22.10.2010
+if nargin<2
+    varargin{1}=[];
+end
+if ~isvarname(get(hObject,'String'))
+    errordlg('Has to be a valid variable Name','Error Dialog','modal')
+    uiwait
+    set(hObject,'UserData',1)
+    return
+elseif exist_in_base_ws(get(hObject,'String'))==1  && ~strcmp(varargin{1},'NoWarning')
+    warndlg('Variable with this name already exists in workspace','Warning','modal')
+    uiwait
+end
+set(hObject,'UserData',0)
