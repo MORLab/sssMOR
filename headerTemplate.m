a1d707a8f3b793b2898f130c224807b74d7d700a
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-08-14 09:21:18 +0200

    new headerTemplate looks more like MATLAB builtin

--- headerTemplate.m
+++ headerTemplate.m
@@ -1,33 +1,34 @@
 function headerTemplate
-% HEADERTEMPLATE - MORLab toolbox header template
-% ------------------------------------------------------------------
-% [outputs] = HEADERTEMPLATE(inputs)
-% Inputs:       * in1: description
-%               * in2: description
-% Outputs:      * out1: description
-% ------------------------------------------------------------------
-% USAGE:  This header file is to be copied and pasted at the beginning
-% of every script and function within the MORLab toolbox for reference.
+%HEADERTEMPLATE  MORLab toolbox header template
 %
-% The description should be kept as short as possible but as long as
-% required to give a good impression of what the function/script is 
-% about and how it should be used.
+% Syntax:
+%   HEADERTEMPLATE
+%   HEADERTEMPLATE(inputs)
+%   outputs = HEADERTEMPLATE(inputs)
 %
-% See also ARNOLDI, RK.
+% Description:
+%   This header file is to be copied and pasted at the beginning
+%   of every script and function within the MORLab toolbox for reference.
+%
+%   The description should be kept as short as possible but as long as
+%   required to give a good impression of what the function/script is 
+%   about and how it should be used.
+%
+% See also:
+%   ARNOLDI, RK
+%
+% References:
+%   [1] Grimme (1997), Krylov Projection Methods for Model Reduction
 %
 % ------------------------------------------------------------------
-% REFERENCES:
-% [1] Grimme (1997), Krylov Projection Methods for Model Reduction
+%   This file is part of sssMOR, a Sparse State Space, Model Order
+%   Reduction and System Analysis Toolbox developed at the Institute 
+%   of Automatic Control, Technische Universitaet Muenchen.
+%   For updates and further information please visit www.rt.mw.tum.de
+%   For any suggestions, submission and/or bug reports, mail us at
+%                     -> sssMOR@rt.mw.tum.de <-
 % ------------------------------------------------------------------
-% This file is part of MORLab, a Sparse State Space, Model Order
-% Reduction and System Analysis Toolbox developed at the Institute 
-% of Automatic Control, Technische Universitaet Muenchen.
-% For updates and further information please visit www.rt.mw.tum.de
-% For any suggestions, submission and/or bug reports, mail us at
-%                   -> MORLab@rt.mw.tum.de <-
-% ------------------------------------------------------------------
-% Authors:      Alessandro Castagnotto, Maria Cruz Varona
-%               HiWi
-% Last Change:  10 Jul 2015
+% Authors:      Alessandro Castagnotto
+% Last Change:  13 Aug 2015
 % Copyright (c) 2015 Chair of Automatic Control, TU Muenchen
 % ------------------------------------------------------------------
