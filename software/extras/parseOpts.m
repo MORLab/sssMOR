0251e3e91fb69088c64da94355c16b5396b75ba7
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-09-01 10:57:39 +0200

    naming convetion parseOpts

--- software/extras/parseOpts.m
+++ software/extras/parseOpts.m
@@ -1,21 +1,21 @@
-function opts = parseOpts(opts,def)
+function Opts = parseOpts(Opts,Def)
 %PARSEOPTS Summary of this function goes here
 %   Detailed explanation goes here
 
-fnames = fieldnames(def); %target field names
+fnames = fieldnames(Def); %target field names
 
 for k = 1:length(fnames)
-    if isstruct(def.(fnames{k})) %nested structure
+    if isstruct(Def.(fnames{k})) %nested structure
         % call parseOpts with the nested structure!
           % make sure the nested structure is defined 
-            if ~isfield(opts,fnames{k})
-                opts.(fnames{k}) = struct;
+            if ~isfield(Opts,fnames{k})
+                Opts.(fnames{k}) = struct;
             end
           % call the function again
-            opts.(fnames{k}) = parseOpts(opts.(fnames{k}),def.(fnames{k}));
+            Opts.(fnames{k}) = parseOpts(Opts.(fnames{k}),Def.(fnames{k}));
     else
-        if ~isfield(opts,(fnames{k})) || isempty(opts.(fnames{k}))
-            opts.(fnames{k}) = def.(fnames{k});
+        if ~isfield(Opts,(fnames{k})) || isempty(Opts.(fnames{k}))
+            Opts.(fnames{k}) = Def.(fnames{k});
         end
     end
 end
