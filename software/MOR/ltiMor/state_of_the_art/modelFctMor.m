93c7c5683347aa361c2612ba9d493092d2fb7e56
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-09-15 13:40:11 +0200

    modelFctMor updated to avoid rk
    So far it works only for reduced orders 2 (like spark)

--- software/MOR/ltiMor/state_of_the_art/modelFctMor.m
+++ software/MOR/ltiMor/state_of_the_art/modelFctMor.m
@@ -62,6 +62,7 @@ function [sysr, s0new] = modelFctMor(sys,redFct,varargin)
     Def.maxiter = 20;
     Def.tol     = 1e-6;
     Def.verbose = 0;
+    Def.update  = '';
 
     if ~exist('Opts','var') || isempty(Opts)
         Opts = Def;
@@ -71,6 +72,12 @@ function [sysr, s0new] = modelFctMor(sys,redFct,varargin)
     %%  Computations
 
     s0m = Opts.s0m; %initialize
+    [sysm,V,W] = rk(sys,s0m,s0m);
+    
+    %   Initialize variables in nested functions
+    L1 = zeros(size(sys.A));U1=L1;P1=L1;Q1=L1; 
+    L2 = zeros(size(sys.A));U2=L2;P2=L2;Q2=L2; 
+    
     stop = 0;
     kIter = 0;
 
@@ -78,13 +85,24 @@ function [sysr, s0new] = modelFctMor(sys,redFct,varargin)
 
     while ~stop
         kIter = kIter + 1; if Opts.verbose, fprintf(sprintf('modelFctMor: k=%i\n',kIter));end
-        % update model
-        if length(s0m)<size(sys.a,1)
-            sysm = rk(sys,s0m,s0m);
-        else
-            warning(['Model function is already as big as the original model.',...
-                ' Using the original model for one last iteration']);
-            sysm = sys;
+        if kIter > 1
+            % update model
+            if length(s0m)<size(sys.a,1)
+                %   Update model function
+                switch Opts.update
+                    case 'rk'
+                        sysm = rk(sys,s0m,s0m);
+                    otherwise
+                        computeLU(s0);  
+                        V = newColV(V, 2);  W = newColW(W, 2);
+                        sysm = sss(W'*sys.A*V,W'*sys.B,sys.C*V,...
+                            zeros(size(sys.C,1),size(sys.B,2)),W'*sys.E*V);
+                end
+            else
+                warning(['Model function is already as big as the original model.',...
+                    ' Using the original model for one last iteration']);
+                sysm = sys;
+            end
         end
         % reduction of new model with new starting shifts
         [sysr, s0new] = redFct(sysm,s0);
@@ -111,5 +129,53 @@ function [sysr, s0new] = modelFctMor(sys,redFct,varargin)
         end
         if crit <= Opts.tol, stop = 1;
         elseif length(s0m)> size(sys.a,1),stop = 1;end
+    end
+    
+    %%  Functions copied from "spark"
+    function computeLU(s0)
+        % compute new LU decompositions
+        if real(s0(1))==real(s0(2))  % complex conjugated or double shift
+            [L1,U1,P1,Q1] = lu(sparse(sys.A-s0(1)*sys.E));  L2=conj(L1);U2=conj(U1);P2=P1;Q2=Q1;
+        else                         % two real shifts
+            [L1,U1,P1,Q1] = lu(sparse(sys.A-s0(1)*sys.E));  [L2,U2,P2,Q2] = lu(sparse(sys.A-s0(2)*sys.E));
+        end
+    end
+    function V = newColV(V, k)
+        % add columns to input Krylov subspace
+        for i=(size(V,2)+1):2:(size(V,2)+2*k)
+            if i==1, x=sys.B; else x=sys.E*V(:,i-1); end
+            r1  = Q1*(U1\(L1\(P1*x)));   tmp = Q2*(U2\(L2\(P2*x)));
+            v1 = real(0.5*r1 + 0.5*tmp); v2  = real(Q2*(U2\(L2\(P2*(sys.E*r1)))));
+            V = GramSchmidt([V,v1,v2],[],[],[i,i+1]);
+        end
+    end
+    function W = newColW(W, k)
+        % add columns to output Krylov subspace
+        for i=(size(W,2)+1):2:(size(W,2)+2*k)
+            if i==1, x=sys.C; else x=W(:,i-1)'*sys.E; end
+            l1  = x*Q1/U1/L1*P1;          tmp = x*Q2/U2/L2*P2;
+            w1 = real(0.5*l1 + 0.5*tmp);  w2  = real(l1*sys.E*Q2/U2/L2*P2);
+            W = GramSchmidt([W,w1',w2'],[],[],[i,i+1]);
+        end
+    end
+    function [X,Y,Z] = GramSchmidt(X,Y,Z,cols)
+        % Gram-Schmidt orthonormalization
+        %   Input:  X,[Y,[Z]]:  matrices in Sylvester eq.: V,S_V,Crt or W.',S_W.',Brt.'
+        %           cols:       2-dim. vector: number of first and last column to be treated
+        %   Output: X,[Y,[Z]]:  solution of Sylvester eq. with X.'*X = I
+        % $\MatlabCopyright$
+
+        if nargin<4, cols=[1 size(X,2)]; end
+        for k=cols(1):cols(2)
+            for j=1:(k-1)                       % orthogonalization
+                T = eye(size(X,2)); T(j,k)=-X(:,k)'*X(:,j);
+                X = X*T;
+                if nargout>=2, Y=T\Y*T; end
+                if nargout>=3, Z=Z*T; end
+            end
+            h = norm(X(:,k));  X(:,k)=X(:,k)/h; % normalization
+            if nargout>=2, Y(:,k) = Y(:,k)/h; Y(k,:) = Y(k,:)*h; end
+            if nargout==3, Z(:,k) = Z(:,k)/h; end
+        end
     end
 end
