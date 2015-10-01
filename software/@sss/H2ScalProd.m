a0e1f9eb58d651a99fc552fad64fab850473a0d8
Maria Cruz Varona <maria.cruz@tum.de>
2015-10-01 12:30:30 +0200

    first commit submodule

--- software/@sss/H2ScalProd.m
+++
@@ -1,8 +0,0 @@
-% H2 inner product  - alpha-Version!!
-
-function ip = H2ScalProd(sys1, sys2)
-if sys1.is_dae, error, end
-if sys2.is_dae, error, end
-
-ip = trace(sys1.B'*lyap(sys1.A',sys2.A,sys1.C'*sys2.C)*sys2.B);
-
