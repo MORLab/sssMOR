f7860b802cc93f8fb3b962f883bcc6fa44a034d8
Maria Cruz Varona <maria.cruz@tum.de>
2015-11-07 17:49:34 +0100

    Capital after name of a function

--- doc/source/functions_index.m
+++ doc/source/functions_index.m
@@ -5,7 +5,7 @@
 %
 % * <.\arnoldihelp.html |arnoldi|>  --  Arnoldi algorithm using multiple expansion points
 % * <.\irkahelp.html |irka|>  --  Iterative Rational Krylov Algorithm
-% * <.\isH2opthelp.html |isH2opt|>  --  evaluate Maier-Luenberger conditions for H2-optimality
+% * <.\isH2opthelp.html |isH2opt|>  --  Evaluate Maier-Luenberger conditions for H2-optimality
 % * <.\modalMorhelp.html |modalMor|>  --  Modal model order reduction of LTI SISO systems
 % * <.\momentshelp.html |moments|>  --  Returns the moments or Markov parameters of an LTI system
 % * <.\rkhelp.html |rk|>  --  Model Order Reduction by Rational Krylov
@@ -14,13 +14,13 @@
 %% LTI MOR State of the Art
 %
 % * <.\curehelp.html |cure|>  --  CUmulative REduction framework
-% * <.\porkVhelp.html |porkV|>  --  Pseudo-Optimal Rational (Input) Krylov PORK [Wolf et al. 2013]
-% * <.\porkWhelp.html |porkW|>  --  Pseudo-Optimal Rational (Output) Krylov PORK [Wolf et al. 2013]
+% * <.\porkVhelp.html |porkV|>  --  Pseudo-Optimal Rational Krylov (Input)
+% * <.\porkWhelp.html |porkW|>  --  Pseudo-Optimal Rational Krylov (Output)
 % * <.\sparkhelp.html |spark|>  --  Stability Preserving Adaptive Rational Krylov
 
 %% Sparse State Space
 %
-% * <.\appendhelp.html |append|>  --  Appends a set of sparse LTI system (sss)
+% * <.\appendhelp.html |append|>  --  Appends a set of sparse LTI systems (sss)
 % * <.\bodehelp.html |bode|>  --  Plots the bode diagram of an LTI system
 % * <.\c2dhelp.html |c2d|>  --  Converts a sss object from continues to discrete
 % * <.\clearhelp.html |clear|>  --  Deletes all state-space dimension related properties
@@ -30,11 +30,11 @@
 % * <.\diaghelp.html |diag|>  --  Transforms an LTI system to (block)-diagonal representation
 % * <.\disphelp.html |disp|>  --  Displays information about a sparse state-space model
 % * <.\eighelp.html |eig|>  --  Compute eigenvalues and eigenvectors of a sparse state-space model
-% * <.\eigshelp.html |eigs|>  --  compute eigenvalues of the sparse state space system using sparse matrices.
+% * <.\eigshelp.html |eigs|>  --  Compute eigenvalues of the sparse state space system using sparse matrices.
 % * <.\freqresphelp.html |freqresp|>  --  Evaluates complex transfer function of LTI systems
 % * <.\impulsehelp.html |impulse|>  --  Computes and/or plots the impulse response of a sparse LTI system
 % * <.\issdhelp.html |issd|>  --  Check strict dissipativity of LTI sss system
-% * <.\isstablehelp.html |isstable|>  --  check stability of LTI sss system
+% * <.\isstablehelp.html |isstable|>  --  Check stability of LTI sss system
 % * <.\normhelp.html |norm|>  --  Computes the p-norm of an sss LTI system
 % * <.\plushelp.html |plus|>  --  Computes sum of two LTI systems: u-->(sys1+sys2)-->y
 % * <.\pzmaphelp.html |pzmap|>  --  Pole-zero plot of sparse state-space system
@@ -46,12 +46,12 @@
 % * <.\simForwardEulerhelp.html |simForwardEuler|>  --  Integrates sss model using forward Euler
 % * <.\simRK4help.html |simRK4|>  --  Integrates sss model using Runge-Kutta 4
 % * <.\sizehelp.html |size|>  --  Computes the size of a sparse LTI system (sss)
-% * <.\spyhelp.html |spy|>  --  plot sparsity pattern of sss system
+% * <.\spyhelp.html |spy|>  --  Plot sparsity pattern of sss system
 % * <.\sshelp.html |ss|>  --  Converts sparse LTI system (sss) to Matlab\control\ss
 % * <.\stephelp.html |step|>  --  Computes and/or plots the step response of a sparse LTI system
-% * <.\truncatehelp.html |truncate|>  --  truncates a sparse LTI system (sss)
+% * <.\truncatehelp.html |truncate|>  --  Truncates a sparse LTI system (sss)
 % * <.\minushelp.html |minus|>  --  Computes difference of two LTI systems: u-->(sys1-sys2)-->y
-% * <.\mtimeshelp.html |mtimes|>  --  mtimes computes the product of two LTI systems: u-->sys2-->sys1-->y
+% * <.\mtimeshelp.html |mtimes|>  --  Computes the product of two LTI systems: u-->sys2-->sys1-->y
 
 %% Header Templates
 %
