fd14a65a461c260b62c8e762145033178fd89d0b
Lisa Jeschek <lisa.jeschek@tum.de>
2015-11-07 17:42:54 +0100

    Added testImpulse.m

--- test/testScripts/test.m
+++ test/testScripts/test.m
@@ -64,13 +64,14 @@ suite18=TestSuite.fromFile('testBode.m');
 suite19=TestSuite.fromFile('testEig.m');
 suite20=TestSuite.fromFile('testEigs.m');
 suite21=TestSuite.fromFile('testFreqresp.m');
+suite22=TestSuite.fromFile('testImpulse.m');
 
 % suite11=TestSuite.fromFile('testSSS.m'); %not yet
 
 % Add/remove suiteX (e.g. [suite1, suite3] to run testArnoldi and testIRKA)
 suite=[suite1,suite2,suite3,suite4,suite5,suite6,suite7,suite8,suite9,suite10,...
 suite11,suite12,suite13,suite14,suite15,suite16, suite17, suite18, suite19,...
-suite20, suite21];
+suite20, suite21, suite22];
 
 
 %% Run and show results
