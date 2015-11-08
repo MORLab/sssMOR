06e7846876891b54f8c71104f213a1af512abf8d
Lisa Jeschek <lisa.jeschek@tum.de>
2015-11-08 23:14:47 +0100

    Added testIsstable.m Updated testImpulse.m and test.m

--- test/testScripts/testIsstable.m
+++ test/testScripts/testIsstable.m
@@ -1,4 +1,4 @@
-classdef testImpulse < matlab.unittest.TestCase
+classdef testIsstable < matlab.unittest.TestCase
     
     properties
          sysCell;
@@ -30,23 +30,17 @@ classdef testImpulse < matlab.unittest.TestCase
     
     %Test functions
     methods (Test)  
-        function testImpulse1(testCase)
+        function testIsstable1(testCase)
             for i=1:length(testCase.sysCell)
                 sys=testCase.sysCell{i};
-                t=1:0.1:5;
                 
-                [actH]=impulse(sys,t); 
-                [expH]=impulse(ss(sys),t);
-                         
+                [actB] = isstable(sys);
+                [expB]=isstable(ss(sys));
                 
-                actSolution={actH};
-                expSolution={expH};
+                actSolution={logical(actB)};
+                expSolution={expB};
                 
                 verification (testCase, actSolution, expSolution);
-                verifyInstanceOf(testCase, actT , 'double', 'Instances not matching');
-                verifyInstanceOf(testCase, actH , 'double', 'Instances not matching');
-                verifySize(testCase, actH, size(expH), 'Size not matching');
-                verifySize(testCase, actT, size(t), 'Size not matching');
             end
         end
     end
