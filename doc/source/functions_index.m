9b4b6f0c9eb3bcb40ec9107c4e47d21adb827df2
Rodrigo Mancilla <rodrigo.mancilla@tum.de>
2015-11-11 14:46:18 +0100

    Update footer to include logos

--- doc/source/functions_index.m
+++ doc/source/functions_index.m
@@ -3,17 +3,18 @@
 
 %% LTI MOR Classic
 %
-% * <.\arnoldihelp.html |arnoldi|>  --  Arnoldi algorithm using multiple expansion points
+% * <.\arnoldihelp.html |arnoldi|>  --  Arnoldi algorithm for Krylov subspaces with multiple shifts
 % * <.\irkahelp.html |irka|>  --  Iterative Rational Krylov Algorithm
-% * <.\isH2opthelp.html |isH2opt|>  --  Evaluate Maier-Luenberger conditions for H2-optimality
-% * <.\modalMorhelp.html |modalMor|>  --  Modal model order reduction of LTI SISO systems
+% * <.\isH2opthelp.html |isH2opt|>  --  Evaluate Maier-Luenberger necessary conditions for H2-optimality
+% * <.\modalMorhelp.html |modalMor|>  --  Modal model order reduction of LTI systems
 % * <.\momentshelp.html |moments|>  --  Returns the moments or Markov parameters of an LTI system
 % * <.\rkhelp.html |rk|>  --  Model Order Reduction by Rational Krylov
-% * <.\tbrhelp.html |tbr|>  --  Performs model order reduction by the Truncated Balanced Realization
+% * <.\tbrhelp.html |tbr|>  --  Performs model order reduction by Truncated Balanced Realization
 
 %% LTI MOR State of the Art
 %
 % * <.\curehelp.html |cure|>  --  CUmulative REduction framework
+% * <.\getSylvesterhelp.html |getSylvester|>  --  Get matrices of Sylvester's equation for Krylov subspaces
 % * <.\porkVhelp.html |porkV|>  --  Pseudo-Optimal Rational Krylov (Input)
 % * <.\porkWhelp.html |porkW|>  --  Pseudo-Optimal Rational Krylov (Output)
 % * <.\sparkhelp.html |spark|>  --  Stability Preserving Adaptive Rational Krylov
@@ -36,6 +37,8 @@
 % * <.\issdhelp.html |issd|>  --  Check strict dissipativity of LTI sss system
 % * <.\isstablehelp.html |isstable|>  --  Check stability of LTI sss system
 % * <.\normhelp.html |norm|>  --  Computes the p-norm of an sss LTI system
+% * <.\minushelp.html |minus|>  --  Computes difference of two LTI systems: u-->(sys1-sys2)-->y
+% * <.\mtimeshelp.html |mtimes|>  --  Computes the product of two LTI systems: u-->sys2-->sys1-->y
 % * <.\plushelp.html |plus|>  --  Computes sum of two LTI systems: u-->(sys1+sys2)-->y
 % * <.\pzmaphelp.html |pzmap|>  --  Pole-zero plot of sparse state-space system
 % * <.\residuehelp.html |residue|>  --  Computes residues, poles and feedthrough of an LTI system
@@ -50,8 +53,6 @@
 % * <.\sshelp.html |ss|>  --  Converts sparse LTI system (sss) to Matlab\control\ss
 % * <.\stephelp.html |step|>  --  Computes and/or plots the step response of a sparse LTI system
 % * <.\truncatehelp.html |truncate|>  --  Truncates a sparse LTI system (sss)
-% * <.\minushelp.html |minus|>  --  Computes difference of two LTI systems: u-->(sys1-sys2)-->y
-% * <.\mtimeshelp.html |mtimes|>  --  Computes the product of two LTI systems: u-->sys2-->sys1-->y
 
 %% Header Templates
 %
@@ -61,7 +62,7 @@
 %%
 % <html>
 %   <hr>
-%   <p class="copy">&copy; 2015 RT Technische Universität München
+%   <p class="copy">&copy; 2015 RT Technische Universit&auml;t M&uuml;nchen
 %        <tt class="minicdot">&#149;</tt>
 %        <a href="http://www.rt.mw.tum.de">Website</a>
 %        <tt class="minicdot">&#149;</tt>
@@ -69,4 +70,18 @@
 %        <tt class="minicdot">&#149;</tt>
 %        <a href="file:./README.txt">Read me</a>
 %   </p>
+%   <div style="position: relative; width:100%; height: 100px;">
+%        <div style="position: absolute; left:0%;">
+%             <img src="img/logo_sssMOR_long.png" alt="sssMOR_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; left:25%;">
+%             <img src="img/MORLAB_Logo.jpg" alt="MORLAB_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; left:60%;">
+%             <img src="img/Logo_Textzusatz_rechts_engl_Chair.png" alt="RT_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; right:0%;">
+%             <img src="img/TUM-logo.png" alt="TUM_Logo" height="40px">
+%        </div>
+%   </div>
 % </html>
