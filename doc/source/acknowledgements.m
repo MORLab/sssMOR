9d1b0f72b460fc3bdce31127ee32d0e7b8c7a6ed
Rodrigo Mancilla <rodrigo.mancilla@tum.de>
2015-11-12 21:43:00 +0100

    Add an Acknowledgements page

---
+++ doc/source/acknowledgements.m
@@ -0,0 +1,43 @@
+%% Ackowledgements
+%
+% The sss class and all of its related analysis functions were created at
+% the Chair of Automatic Control, TU Munich, by former researchers like
+% Rudy Eid, Heiko Peuscher and Silvia Cremer and is currently being
+% serviced and developed in collaboration with the Chair of Thermofluid
+% Dynamics.
+%
+% We would like thank all the people that diretly or indirectly contributed
+% to the conception of this toolbox. Special thanks to:
+%
+% * Nico Walz
+% * HiWis
+% * Nico Walz
+% * Nico Walz
+% * Nico Walz
+
+%%
+% <html>
+%   <hr>
+%   <p class="copy">&copy; 2015 RT Technische Universit&auml;t M&uuml;nchen
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="https://www.rt.mw.tum.de/?sssMOR">Website</a>
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="file:txts/LICENSE.txt">License</a>
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="file:txts/RELEASE.txt">Release Notes</a>
+%   </p>
+%   <div style="position: relative; width:100%; height: 100px;">
+%        <div style="position: absolute; left:0%;">
+%             <img src="img/logo_sssMOR_long.png" alt="sssMOR_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; left:20%;">
+%             <img src="img/MORLAB_Logo.jpg" alt="MORLAB_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; right:25%;">
+%             <img src="img/Logo_Textzusatz_rechts_engl_Chair.png" alt="RT_Logo" height="40px">
+%        </div>
+%        <div style="position: absolute; right:0%;">
+%             <img src="img/TUM-logo.png" alt="TUM_Logo" height="40px">
+%        </div>
+%   </div>
+% </html>
