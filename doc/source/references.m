407e476ecb936869280e99287091702b84dc4efe
Maria Cruz Varona <maria.cruz@tum.de>
2015-11-13 17:34:20 +0100

    References added!

--- doc/source/references.m
+++ doc/source/references.m
@@ -1,6 +1,31 @@
 %% References
 %
-% *[1] Grimme (1997)*, Krylov projection methods for model reduction
+%       * *[1] Antoulas (2005)*, Approximation of large-scale dynamical systems
+%       * *[2] Antoulas (2010)*, Interpolatory model reduction of large-scale...
+%       * *[3] Beattie et al. (2014)*, Model reduction by rational interpolation
+%       * *[4] Castagnotto et al. (2015)*, Stability-preserving, adaptive
+%              model order reduction of DAEs by Krylov subspace methods
+%		* *[5] Foellinger (2013)*, Regelungstechnik (pp. 305-319)
+%       * *[6] Gallivan et al. (2002)*, Sylvester equations and projection
+%              based model reduction
+%       * *[7] Gear (1971)*, Numerical Initial Value Problems in 
+%       Ordinary Differential Equations.
+%       * *[8] Giraud (2005)*, The loss of orthogonality in the Gram-Schmidt... 
+%       * *[9] Grimme (1997)*, Krylov projection methods for model reduction
+%       * *[10] Gugercin et al. (2008)*, H2 model reduction for large-scale linear dynamical systems
+%		* *[11] Lehoucq and Sorensen (1996)*, Deflation Techniques for an 
+%              Implicitly Re-Started Arnoldi Iteration.
+%       * *[12] Moore (1981)*, Principal component analysis in linear systems: controllability,
+%       observability and model reduction
+%       * *[13] Panzer (2014)*, Model Order Reduction by Krylov Subspace Methods
+%              with Global Error Bounds and Automatic Choice of Parameters
+%       * *[14] Shampine and Gordon (1975)*, Computer Solution of Ordinary Differential 
+%       Equations: the Initial Value Problem, W. H. Freeman, San Francisco.
+%       * *[15] Shampine (1994)*, Numerical Solution of Ordinary Differential Equations, 
+%       Chapman & Hall, New York.
+%		* *[16] Sorensen (1992)*, Implicit Application of Polynomial Filters 
+%              in a k-Step Arnoldi Method.
+%       * *[17] Wolf (2014)*, H2 Pseudo-Optimal Moder Order Reduction
 
 %%
 % <html>
