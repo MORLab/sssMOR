0f532fc352ee66e59edcd8c4e811a085bbee5723
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-11-25 17:23:50 -0500

    cirka - without anonymous function for higher speedup

--- software/MOR/ltiMor/stateOfTheArt/cirka.m
+++ software/MOR/ltiMor/stateOfTheArt/cirka.m
@@ -1,21 +1,60 @@
 function [sysr, s0] = cirka(sys, s0, Opts) 
 
-%% Define auxiliary functions
-redFct      = @(sys,s0) irka(sys,s0,struct('stopCrit','s0','suppressverbose',1));
-redFctOut   = @(sys,s0) getDesiredOutput(redFct,[1,4],sys,s0);
+    %% Define execution options
+    Def.qm0     = length(s0)+2;
+    Def.s0m     = zeros(1,Def.qm0); 
+    Def.maxiter = 8; Def.tol = 1e-3;
+    Def.verbose = 0; Def.plot = 0;
+    Def.updateModel = 'new';
+    Def.irka.stopCrit = 's0';
+    Def.irka.suppressverbose = 1;
+%     Def.irka.maxiter = 20;
+%     Def.irka.tol = 1e-2; 
 
-%% Define execution options
-Def.qm0     = length(s0)+2;
-Def.s0m     = zeros(1,Def.qm0); 
-Def.maxiter = 20; Def.tol = 1e-3;
-Def.verbose = 0; Def.plot = 0;
-Def.updateModel = 'new';
+    if ~exist('Opts','var') || isempty(Opts)
+        Opts = Def;
+    else
+        Opts = parseOpts(Opts,Def);
+    end 
+    
+    %% run computations
+    stop = 0;
+    kIter = 0;
+    
+    %   Generate the model function
+    s0m = Opts.s0m;    [sysm, s0mTot, V, W] = modelFct(sys,s0m);
 
-if ~exist('Opts','var') || isempty(Opts)
-    Opts = Def;
-else
-    Opts = parseOpts(Opts,Def);
-end 
+    if Opts.verbose, fprintf('Starting model function MOR...\n'); end
 
-%% Perform reduction
-[sysr,s0] = modelFctMor(sys,redFctOut,s0,Opts);
+    while ~stop
+        kIter = kIter + 1; if Opts.verbose, fprintf(sprintf('modelFctMor: k=%i\n',kIter));end
+        if kIter > 1
+            % update model
+            [sysm, s0mTot, V, W] = modelFct(sys,s0,s0mTot,V,W,Opts);
+        end
+        % reduction of new model with new starting shifts
+        [sysr, ~,~, s0new] = irka(sysm,s0,Opts.irka);
+        % computation of convergence
+        if stoppingCrit
+            stop = 1;
+        else
+            %Overwrite parameters with new variables
+            s0 = s0new;    
+        end
+        if kIter > Opts.maxiter; 
+            warning('modelFctMor did not converge within maxiter'); 
+            return
+        end
+    end
+
+    function stop = stoppingCrit
+        stop = 0;
+        if norm(s0) == 0
+            crit = norm(setdiffVec(s0new,s0)); %absolute
+        else
+            crit = norm(setdiffVec(s0new,s0))/norm(s0); %relative
+        end
+        if crit <= Opts.tol, stop = 1;
+        elseif length(s0mTot)> size(sys.a,1),stop = 1;end
+    end    
+end
