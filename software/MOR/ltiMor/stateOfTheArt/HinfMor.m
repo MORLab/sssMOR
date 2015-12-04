f9b056ca91ae98602472cf48787fe6f692451eae
Alessandro Castagnotto <a.castagnotto@tum.de>
2015-12-04 09:36:36 -0500

    HinfMor - WIP
    Refactoring, adding new functionalities as discussed with Serkan and Chris
    
    Modifications are not over yet

--- software/MOR/ltiMor/stateOfTheArt/HinfMor.m
+++ software/MOR/ltiMor/stateOfTheArt/HinfMor.m
@@ -1,8 +1,8 @@
-function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin) 
+function [sysr, Hinf, sysr0, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin) 
     % HINFMOR - H-infinity reduction by tangential interpolation
     % ------------------------------------------------------------------
-    %
-    % [sysr, Hinf] = HINFMOR(sys, varargin) 
+    % TODO
+    % sysr = HINFMOR(sys, varargin) 
     % Inputs:       * sys: full oder model (sss)
     %               * Opts (opt.) structure with execution parameters
     % Outputs:      * sysr: reduced order model (sss)
@@ -24,12 +24,37 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
     % Copyright (c) ?
     % ------------------------------------------------------------------
 
+    
+    %%  Input parsing and execution parameters
+    if ~isempty(varargin) 
+        if isstruct(varargin{1}) %Opts passsed
+            Opts = varargin{1};
+        else %Opts.corrType passed
+            Opts.corrType = varargin{1};        
+        end
+    else
+        Opts = struct();
+    end
+    
+    Def.plotCostOverDr = 0;
+    Def.irka    = []; %run irka with defaul parameters
+    Def.corrType = 'normOpt_cicle';
+    Def.DrInit  = 0; %0, Ge0, matchGe0, maxGe
+    Def.plot    = 0; % generate analysis plot
+    
+    % create the options structure
+    if ~exist('Opts','var') || isempty(Opts)
+        Opts = Def;
+    else
+        Opts = parseOpts(Opts,Def);
+    end
+    
     %%  Run IRKA
     if sys.isSiso
         % initialize
         try s0 = -eigs(sys,n,'sm').'; catch , s0 = zeros(1,n); end
         % run IRKA
-        [sysr0, ~, ~, ~, ~, ~, ~, B_, Rt, C_, Lt] = irka(sys,s0);
+        [sysr0, ~, ~, ~, ~, ~, ~, B_, Rt, C_, Lt] = irka(sys,s0,Opts.irka);
     else %MIMO
         % initialize
         %   compute one step of tangential Krylov at 0 to get initial tangent 
@@ -38,7 +63,7 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
         sysr = rk(sys,s0,s0,Rt,Lt);  [X,D,Y] = eig(sysr);
         Rt = full((Y.'*sysr.B).'); Lt = full(sysr.C*X); s0 = -diag(D).';
         %run IRKA
-        [sysr0, ~, ~, ~, ~, ~, ~, B_, Rt, C_, Lt] = irka(sys,s0,Rt,Lt);
+        [sysr0, ~, ~, ~, ~, ~, ~, B_, Rt, C_, Lt] = irka(sys,s0,Rt,Lt,Opts.irka);
     end
 
     %   Transform (A- s0*E) to (s0*E- A)
@@ -51,7 +76,6 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
     %     warning('Residuals could be wrong')
     %     keyboard
     % end
-
 
     %%  Make Hinf correction
     %
@@ -66,27 +90,15 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
     % DRange:       computes the actual error norm for a series of feedtrhoughs
     %               and takes the one with minimum value
 
-    if nargin>2
-        corrType = varargin{1};
-    else
-        corrType = 'normOpt';
-    end
-
-    %   Parametrize reduced order models by Dr
-    sysrfun = @(Dr) sss(sysr0.A+Lt.'*Dr*Rt, sysr0.B+Lt.'*Dr, ...
-                                     sysr0.C+Dr*Rt, Dr, sysr0.E);
-
-    switch corrType
+    switch Opts.corrType
         case 'steadyState'
-            warning(['This approach fails due to the same reasons stated for ',...
-                'the forAll case']);
+            warning('this option is obsolete and shows bad performance');
             %plus, Ge(0) changes depending on Dr
 
-            Dr = freqresp(sys,0)-freqresp(sysr0,0);
-            sysr = sysrfun(Dr);
+            DrOpt = DrInit('Ge0');
+            sysr = sysrfun(DrOpt);
         case 'steadyStateOpt'
-            warning(['This approach fails due to the same reasons stated for ',...
-                'the forAll case']);
+            warning('this option is obsolete and shows bad performance');
             % however, is seems to work fine for build...
 
             G0 = freqresp(sys,0); %the only costly part
@@ -95,66 +107,37 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
             cost = @(Dr) abs(...
                         abs(G0-...
                         freqresp(sysrfun(Dr),0)) - abs(Dr));
-            DrOpt = fmincon(cost,Dr0)
+            DrOpt = fmincon(cost,Dr0);
             sysr = sss(sysr0.A+Lt.'*DrOpt*Rt, sysr0.B-Lt.'*DrOpt, ...
                        sysr0.C-DrOpt*Rt, Dr0, sysr0.E);
         case 'findGe0match'
-            warning(['This approach fails since there does not seem to be a ',...
-                'reduced order model that yields a completely flat magnitude',...
-                'response in the error']);
+            warning('this option is obsolete and shows bad performance');
 
-            G0 = freqresp(sys,0); %the only costly part
-
-            Dr0 = G0 - freqresp(sysr0,0) %get an initial feedthrough
-            deltaDr = 100*abs(Dr0), nStep = 5000; DrSet = []; dSet = [];
-            syse0 = sys-sysr0;
-            Hinf0 = norm(syse0,Inf); d0 = abs(Dr0); dMin = d0; %initial error
-            drawnow
-            for Dr = Dr0-deltaDr:(2*deltaDr)/nStep:Dr0+deltaDr;
-                sysr = sysrfun(Dr);
-
-                d = abs(abs(G0-freqresp(sysr,0)) - abs(Dr));
-                if d < dMin
-                    dMin = d;
-                    dSet = [dSet, d];
-                    DrSet = [DrSet,Dr];
-    %                 syse = sys-sysr; sigma(syse,'Color',rand(1,3));
-    %                 drawnow, keyboard
-                end
-            end
-            %   best feedthrough in term of minimizing the error between the
-            %   response at 0 and Inf
-            Dr = DrSet(end), dMin
+            DrOpt = DrInit('matchGe0');
             sysr = sysrfun(Dr);
-        case 'normOpt'
-            Dr0 = zeros(sys.p,sys.m);
-            [sysr, ~, Hinf,tOpt] = normOpt(Dr0);
-        case 'steadyState+normOpt'
-            % execution params
-            plotCostOverDr = 1;
-            
-            % initialization at steady-state error amplitude response
-            G0 = freqresp(sys,0); Dr0 = G0-freqresp(sysr0,0);
-            [sysr, DrOpt, Hinf,tOpt] = normOpt(Dr0);
-
-            %   See how the cost behaves around the chosen minimum?
-            if plotCostOverDr
-                nStep = 20; kRange = 6;
-                if isscalar(DrOpt) %SISO
-                    DrRange = linspace(DrOpt*(1-kRange),DrOpt*(1+kRange), nStep); 
-                elseif any (size(DrOpt) == 1) % SIMO or MISO
-                    DrRange = cell(size(DrOpt)); %get the right size
-                    DrRange1 = linspace(DrOpt(1)*(1-kRange),DrOpt(1)*(1+kRange), nStep);
-                    DrRange2 = linspace(DrOpt(2)*(1-kRange),DrOpt(2)*(1+kRange), nStep);
-                    [DrRange1, DrRange2] = meshgrid(DrRange1,DrRange2);
-                    DrRange{1} = DrRange1; DrRange{2} = DrRange2;
+        case 'normOpt_siso'
+            warning('this option is obsolete and shows bad performance');
+             % running optimization wr to each entry of D individually
+            % the cost function takes into account only the SISO tf
+            DrOpt = zeros(sys.p,sys.m); tOpt = 0;
+            for iOut = 1:sys.p
+                for jIn = 1:sys.m
+                    Dr0 = 0; 
+                    sysrCurr = @(Dr) sss(....
+                                    sysr0.A+Lt(iOut,:)'*Dr*Rt(jIn,:),...
+                                    sysr0.B(:,jIn)+Lt(iOut,:).'*Dr, ...
+                                    sysr0.C(iOut,:)+Dr*Rt(jIn,:), Dr, sysr0.E);
+                    cost = @(Dr) norm(sys(iOut,jIn)-sysrCurr(Dr),Inf);
+                    [DrOptCurr, ~, tOptCurr] = normOpt(Dr0,cost);
+                    tOpt = tOpt + tOptCurr;
+                    DrOpt(iOut,jIn) = DrOptCurr;
                 end
-                plotOverDrRange(DrOpt,Hinf);
             end
+            sysr = sysrfun(DrOpt);
+            Hinf = norm(sys-sysr)/norm(sys);
         case 'DrRange'
             % Get steady state response of the error system
-            G0 = freqresp(sys,0); 
-            Dr0 = abs(G0 - freqresp(sysr0,0));
+            Dr0 = DrInit('Ge0');
 
             % Define a range for the feedthrough
             nStep = 100;
@@ -162,19 +145,118 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
 
             % Run the actual function
             plotOverDrRange;
+        case 'normOpt'
+            Dr0 = DrInit(Opts.DrInit);
+            cost = @(Dr) norm(sys-sysrfun(Dr),Inf);
+            [DrOpt, Hinf,tOpt] = normOpt(Dr0,cost);
+            sysr = sysrfun(DrOpt);         
+        case 'normOpt_cicle'
+            % running optimization wr to each entry of D individually
+            % the cost function takes into account the whole MIMO system
+            DrOpt = DrInit(Opts.DrInit); HinfVec = norm(sys-sysr0,Inf); tOpt = 0;
+            nCycles = 3; cycleCount = 0; stop = 0; %max number of cycles defined
+            while cycleCount < nCycles && ~stop;
+            cycleCount = cycleCount+1;
+            for iOut = 1:sys.p
+                for jIn = 1:sys.m
+                    Dr0 = DrOpt(iOut,jIn);
+                    cost = @(Dr) norm(sys-sysrfun(Dr,iOut,jIn,DrOpt),Inf);
+                    [DrOptCurr, Hinf,tOptCurr] = normOpt(Dr0,cost);
+                    tOpt = tOpt+tOptCurr;
+                    DrOpt(iOut,jIn) = DrOptCurr;
+                    HinfVec = [HinfVec, Hinf];
+                    if length(HinfVec) > sys.p*sys.m +1 %cycled already once
+                        crit = mean(abs(diff(HinfVec(end-2:end))./HinfVec(end-2:end-1)));
+                        %mean improvement less that 1%
+                        if crit < 1e-2; stop = 1; break, end
+                    end
+                end
+                if stop, break, end %breaking out of nested for loop
+            end
+            end
+            sysr = sysrfun(DrOpt); 
+            figure; plot(0:1:length(HinfVec)-1,HinfVec/HinfVec(1)); ylabel('relative error decrease');
+
         otherwise
             error('Specified Hinf optimization type not valid');
     end
+    
+    %   See how the cost behaves around the chosen minimum?
+    if Opts.plotCostOverDr
+        nStep = 20; kRange = 6;
+        if isscalar(DrOpt) %SISO
+            DrRange = linspace(DrOpt*(1-kRange),DrOpt*(1+kRange), nStep); 
+        elseif any (size(DrOpt) == 1) % SIMO or MISO
+            DrRange = cell(size(DrOpt)); %get the right size
+            DrRange1 = linspace(DrOpt(1)*(1-kRange),DrOpt(1)*(1+kRange), nStep);
+            DrRange2 = linspace(DrOpt(2)*(1-kRange),DrOpt(2)*(1+kRange), nStep);
+            [DrRange1, DrRange2] = meshgrid(DrRange1,DrRange2);
+            DrRange{1} = DrRange1; DrRange{2} = DrRange2;
+        end
+        plotOverDrRange(DrOpt,Hinf);
+    end
 
     %% Additional outputs
-    if nargout > 3
-        Hinf = norm(sys-sysr,inf)/norm(sys,inf);
-        if nargout > 4
+    if nargout > 2
+        HinfO = norm(sys,inf); %original
+        Hinf = norm(sys-sysr,inf)/HinfO; %optimized
+        HinfRatio = Hinf/norm(sys-sysr0,inf)*HinfO; %ratio to irka ROM
+        if nargout > 5
             bound = HinfBound(sys,B_,C_);
         end
     end
 
     %% Auxiliary
+    function Dr0 = DrInit(type)
+        %   Initialize Dr for optimization
+        switch type
+            case '0'
+                Dr0 = zeros(sys.p,sys.m);
+            case 0
+                Dr0 = DrInit('0');    
+            case 'Ge0'   
+                Dr0 = freqresp(sys,0)-freqresp(sysr0,0);      
+            case 'Ge0/2'
+                Dr0 = DrInit('Ge0')/2;
+            case 'matchGe0'                 
+                G0 = freqresp(sys,0); %the only costly part
+
+                Dr0 = DrInit('Ge0'); %get an initial feedthrough
+                deltaDr = 100*abs(Dr0); nStep = 500; DrSet = []; dSet = [];
+                Hinf0 = norm(sys-sysr0,Inf); d0 = DrInit('Ge0'); dMin = d0; %initial error
+                if Opts.plot, figure; sigma(sys-sysr0,'b', 'LineWidth',2); end
+                pause
+                
+                for k = 0:nStep
+                    Dr = Dr0-deltaDr + k*(2*deltaDr)/nStep;
+                    sysr = sysrfun(Dr);
+
+%                     d = abs(abs(G0-freqresp(sysr,0)) - abs(Dr));
+                      d = G0 - (freqresp(sysr,0) + Dr);
+                    if d < dMin
+                        dMin = d;
+                        dSet = [dSet, d];
+                        DrSet = [DrSet,Dr];
+                        if Opts.plot
+                            syse = sys-sysr; sigma(syse,'Color',rand(1,3)); 
+                            drawnow
+                        end
+                    end
+                end
+                %   best feedthrough in term of minimizing the error between the
+                %   response at 0 and Inf
+                Dr0 = DrSet(end); 
+                if Opts.plot 
+                    syse = sys-sysrfun(Dr0); sigma(syse,'Color',rand(1,3),); 
+                    drawnow 
+                end
+                
+            case 'maxGe' 
+                
+            otherwise
+                error('Initialization option for Dr not valid');
+        end
+    end
     function [minDr, minVal] = plotOverDrRange(varargin)
         if isnumeric(DrRange) %vector of Dr values
             %initializing
@@ -290,25 +372,19 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
                 cd(cdir);
             end
     end
-
-    function [sysr, DrOpt, Hinf,tOpt] = normOpt(Dr0)
-        
-            cost = @(Dr) norm(sys-sysrfun(Dr),Inf);
+    function [DrOpt, Hinf,tOpt] = normOpt(Dr0,cost)       
             solver = 'fminsearch';
-            warning('optimizing over the actual error norm');
-            warning off
+%             warning('optimizing over the actual error norm');
             switch solver
                 case 'fminsearch'
                 %zero initialization
-                tic, [DrOpt, Hinf] = fminsearch(cost,Dr0), tOpt = toc
+                tic, [DrOpt, Hinf] = fminsearch(cost,Dr0); tOpt = toc;
 
                 case 'ga'
                     options = gaoptimset('Display','iter','TimeLimit',5*60,...
                         'UseParallel',true, 'PopInitRange',[-1;1]);
                     tic, [DrOpt, Hinf] = ga(cost,1,[],[],[],[],[],[],[],options); tOpt = toc
             end
-            warning on
-            sysr = sysrfun(DrOpt); 
     end
     function bound = HinfBound(sys,B_,C_)
         % Panzer 2014 
@@ -329,6 +405,16 @@ function [sysr, sysr0, Hinf, HinfRatio, tOpt , bound] = HinfMor(sys, n, varargin
                 C_S = (L_S'\(P_S'*C_'))';
                 bound = norm(full(C_S*B_S)) + norm(full(B_S))*norm(full(C_S));
             end
+    end
+    function  sysr = sysrfun(Dr,iOut,jIn,DrMIMO)
+        if nargin == 1
+            %Dr is the full feedthrough
+            DrMIMO = Dr;
+        else
+            DrMIMO(iOut,jIn) = Dr;
+        end
+        sysr= sss(sysr0.A+Lt.'*DrMIMO*Rt, sysr0.B+Lt.'*DrMIMO, ...
+                                     sysr0.C+DrMIMO*Rt, DrMIMO, sysr0.E);
     end
 
 end
