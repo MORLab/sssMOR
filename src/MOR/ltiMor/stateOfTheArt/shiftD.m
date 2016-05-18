51139d3d241b0b677526efae8368be92964ca69b
Alessandro Castagnotto <a.castagnotto@tum.de>
2016-05-18 15:40:38 +0200

    shiftD - major bugfix

--- src/MOR/ltiMor/stateOfTheArt/shiftD.m
+++ src/MOR/ltiMor/stateOfTheArt/shiftD.m
@@ -1,10 +1,12 @@
 function sysD = shiftD(sys,D,L,R)
 
+%assumes V = (A-s0*E)\B
+
 switch class(sys)
     case 'sss'
-        sysD = sss(sys.A-L.'*D*R, sys.B-L.'*D, sys.C-D*R, D, sys.E);
+        sysD = sss(sys.A+L.'*D*R, sys.B+L.'*D, sys.C+D*R, D, sys.E);
     case 'ss'
-        sysD = dss(sys.A-L.'*D*R, sys.B-L.'*D, sys.C-D*R, D, sys.E);
+        sysD = dss(sys.A+L.'*D*R, sys.B+L.'*D, sys.C+D*R, D, sys.E);
     otherwise
         error('sssMOR:shiftD:notSupportedClass','The sys object provided is not supported')
 end
