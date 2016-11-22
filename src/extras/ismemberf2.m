6c07dc92367bcfed7eb20603f9f871cbe626f0f5
Alessandro Castagnotto <a.castagnotto@tum.de>
2016-11-22 20:21:47 +0100

    ismemberf2 - added header

--- src/extras/ismemberf2.m
+++ src/extras/ismemberf2.m
@@ -1,7 +1,35 @@
 function idx = ismemberf2(sNew,sOld,tol)
+% ISMEMBERF2 - determine which elements are within a radius
 %
-%   check which entries of sNew are in a circle of radius sOld*tol from any
-%   entry of sOld
+% Syntax:
+%   idx = ISMEMBERF2(sNew,sOld,tol)
+%
+% Description:
+%   Determine which elements of a vector sNew are already included (within
+%   a relative tolerance tol) in a comparison set sOld.
+% 
+% Input Arguments:  
+%       *Required Input Arguments:*
+%       -sNew:			new set
+%       -sOld:			old set as comparison basis
+%       -tol:           tolerance radius (relative to values)
+%
+%
+% See also:
+%   ismember, modelFct, cirka
+%
+% ------------------------------------------------------------------
+%   This file is part of sssMOR, a Sparse State Space, Model Order
+%   Reduction and System Analysis Toolbox developed at the Institute 
+%   of Automatic Control, Technische Universitaet Muenchen.
+%   For updates and further information please visit www.rt.mw.tum.de
+%   For any suggestions, submission and/or bug reports, mail us at
+%                     -> sssMOR@rt.mw.tum.de <-
+% ------------------------------------------------------------------
+% Authors:      Alessandro Castagnotto
+% Last Change:  22 Nov 2016
+% Copyright (c) 2016 Chair of Automatic Control, TU Muenchen
+% ------------------------------------------------------------------
 
 %   doing it with loops, there might be a faster version
 
