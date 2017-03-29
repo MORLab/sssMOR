116d8c834d8492a273fe8e34f4f34874d4893654
Maria Cruz Varona <maria.cruz@tum.de>
2017-03-29 10:22:22 +0200

    changes in testing_rksm during the debugging process

--- src/MOR/ltiMor/classic/testing_rksm.m
+++ src/MOR/ltiMor/classic/testing_rksm.m
@@ -6,6 +6,7 @@ clearvars -global
 
 % load any benchmark system
 sys = loadSss('fom');
+% sys = sys(1,1)
 % other models: rail_1357, rail_5177, iss, gyro, building, CDplayer, beam,
 %               eady, heat-cont, LF10, random, bips98_606,
 %               SpiralInductorPeec, fom
@@ -24,7 +25,7 @@ p = size(C,1);
 
 %% testing function for several cases
 % initialize method
-method = 3; % 1 for standard rksm cyclic, reusing shifts, onesided, hermite
+method = 1; % 1 for standard rksm cyclic, reusing shifts, onesided, hermite
             % 2 for standard rksm cyclic, reusing shifts, onesided, hermite, tangential directions (MIMO)
             % 3 for standard rksm cyclic, reusing shifts, twosided, hermite
             % 4 for standard rksm cyclic, reusing shifts, twosided, not hermite
@@ -35,8 +36,8 @@ method = 3; % 1 for standard rksm cyclic, reusing shifts, onesided, hermite
             
 % choose computation of residual norm and other options
 Opts.residual  = 'residual_lyap';  
-%Opts.residual  = 'norm_chol';
-Opts.maxiter_rksm = 1000;
+% Opts.residual  = 'norm_chol';
+Opts.maxiter_rksm = 95;
 Opts.maxiter = Opts.maxiter_rksm;
 Opts.rctol = 1e-12; 
 
@@ -51,7 +52,7 @@ switch method
 %             'info',1),'maxiter',Opts.maxiter,'restol',0,'rctol',Opts.rctol,...
 %             'info',1,'norm','fro');
 % 
-%         Opts.lse         = 'gauss'; 
+%         Opts.lse = 'gauss'; 
 %         lseType='solveLse';
 %         oper = operatormanager(lseType);
 %         messOpts.solveLse.lse=Opts.lse;
@@ -59,18 +60,21 @@ switch method
 % 
 %         % get adi shifts
 %         [messOpts.adi.shifts.p,~,~,~,~,~,~,eqn]=mess_para(eqn,messOpts,oper);
-        
+%         s0 = messOpts.adi.shifts.p;
 
         Opts.method = 'rksm';
-        %Opts.rksmnorm = 'fro';
+%         Opts.rksmnorm = 'fro';
         Opts.reduction = 'onesided';
         % compute shifts using irka
-        shifts = zeros(1,8);
-        Rt = ones(m,18);
-        Lt = ones(p,18);
+        shifts = zeros(1,2);
+        Rt = ones(m,2);
+        Lt = ones(p,2);
         [~, ~, ~, s0, Rt, Lt] = irka(sys, shifts,Rt,Lt);
         % call function
+        Opts.shifts = 'mess';
+        tic
         [S,R,data_out] = rksm(sys,s0,Opts);
+        toc
         %[S,R,data_out] = lyapchol(sys,s0,Opts);
         
     case 2
@@ -78,14 +82,18 @@ switch method
         Rt = ones(m,6);
         Lt = ones(p,6);
         [~, ~, ~, s0, Rt, Lt] = irka(sys, shifts,Rt,Lt);
+        tic
         [S,R,data_out] = rksm(A,B,E,s0,Rt,Opts);
+        toc
         
     case 3
         % compute shifts using irka
         shifts = zeros(1,10);
         [~, ~, ~, s0] = irka(sys, shifts);
         % call funtion
+        tic
         [S,R,data_out] = rksm(A,B,C,E,s0,Opts);
+        toc
         %[S,R,data_out] = lyapchol(sys,s0,Opts);
     case 4
         % compute shifts using irka
@@ -95,7 +103,9 @@ switch method
         [~, ~, ~, s0, Rt, Lt] = irka(sys, shifts,Rt,Lt);
         s0_inp = s0(1,1:6);
         s0_out = s0(1,7:12);
+        tic
         [S,R,data_out] = rksm(A,B,C,E,s0_inp,s0_out,Opts);
+        toc
         %[S,R,data_out] = My_rksm(sys,s0_inp,s0_out,Opts);
         
     case 5
@@ -107,7 +117,9 @@ switch method
         s0_out = s0(1,7:12);
         Rt = Rt(1,1:6);
         Lt = Lt(1,1:6);
+        tic
         [S,R,data_out] = rksm(A,B,C,E,s0_inp,s0_out,Rt,Lt,Opts);
+        toc
         %[S,R,data_out] = My_rksm(sys,s0_inp,s0_out,Rt,Lt,Opts);
         
     case 6
@@ -126,7 +138,9 @@ switch method
         %s0(1,2) = eigenwerte(1,1);
         
         Opts.shifts = 'adaptive';
+        tic
         [S,data_out] = rksm(A,B,E,s0,Opts);
+        toc
     case 8
         shifts = zeros(1,2);
         [~, ~, ~, s0] = irka(sys, shifts);
@@ -136,24 +150,26 @@ switch method
         %s0(1,2) = eigenwerte(1,1);
 
         Opts.Shifts = 'adaptive';
+        tic
         [S,R,data_out] = rksm(A,B,C,E,s0,Opts);
+        toc
         
 end
 V = data_out.V_basis;
 W = data_out.W_basis;
 
 %% testing quality of solution
-% if isempty(W)
-%     Pr = S'*S;
-%     P = V*Pr*V';
-%    % P  = Pr'*Pr;
-% else
-%     Pr = W*S*V';
-%     P  = Pr*Pr';
-% end
-%  
-% Y = A*P*E' + E*P*A' + B*B';
-% Y_norm = norm(Y);
+if isempty(W)
+    Pr = S'*S;
+    P = V*Pr*V';
+   % P  = Pr'*Pr;
+else
+    Pr = W*S*V';
+    P  = Pr*Pr';
+end
+ 
+Y = A*P*E' + E*P*A' + B*B';
+Y_norm = norm(Y);
 
 
 %% comparing with other methods
@@ -163,8 +179,11 @@ W = data_out.W_basis;
 % Yhammarling = A*Phammarling*E' + E*Phammarling*A' + B*B';
 % Yhammarling_norm = norm(Yhammarling);
 
-% Opts.method = 'adi';
-% [Sadi] = lyapchol(sys,Opts);
-% Padi = Sadi*Sadi';
-% Yadi = A*Padi*E' + E*Padi*A' + B*B';
-% Yadi_norm = norm(Yadi);
+Opts.method = 'adi';
+Opts.rctol = 0;
+tic
+[Sadi] = lyapchol(sys,Opts);
+toc
+Padi = Sadi*Sadi';
+Yadi = A*Padi*E' + E*Padi*A' + B*B';
+Yadi_norm = norm(Yadi);
