d58d57676c200ee9ef735deaef487dfe20c99722
Paul Heidenreich <paul.heidenreich@tum.de>
2017-03-30 09:12:28 +0200

    renamed function rksm to crksm

--- src/MOR/ltiMor/classic/crksm.m
+++ src/MOR/ltiMor/classic/crksm.m
@@ -1,4 +1,4 @@
-function [S,R,output_data] = rksm(varargin)
+function [S,R,output_data] = crksm(varargin)
 % My_rksm - Solve Laypunov equations with a cummulative rational Krylov
 % subspace method
 %
@@ -184,8 +184,10 @@ if isa(varargin{1},'ss') || isa(varargin{1},'sss') || isa(varargin{1},'ssRed')
     tol = Opts.rctol;
     maxiter = Opts.maxiter_rksm;
 
-    if size(s_inp,2) == 1 && exist('s_inp','var')
-        error('At least two starting shifts are necessary');
+    if exist('s_inp','var')
+        if size(s_inp,2) == 1
+            error('At least two starting shifts are necessary');
+        end
     end
               
 % input of matrices    
@@ -515,10 +517,10 @@ switch Opts.rksm_method
                                   [s0,~,~,~,~,~,~,eqn]=mess_para(eqn,messOpts,oper); s0=s0';
                                   Rt = ones(m,size(s0,2));
                                   Lt = ones(p,size(s0,2));
-                                  [~, ~, ~, s0, Rt, Lt] = irka(sys,s0,Rt,Lt);
+                                  %[~, ~, ~, s0, Rt, Lt] = irka(sys,s0,Rt,Lt);
                                   [s_ma] = make_shiftmatrix(s0,m,p,A);
 
-                                  Opts.shifts = 'cyclic';
+                                  %Opts.shifts = 'cyclic';
                                   % hier das j+1 muss immer gesetzt werden
                                   % fuer einen guten Programmablauf
                                   j = 1;
@@ -531,15 +533,19 @@ switch Opts.rksm_method
                             counter_out = 0;       high_out = false; 
                         end
                     end
-
                     % save last shift,read in next shift, check order for input and output shifts
                     % save last shift
                     if ii <= length(s_ma(1,:))
                         jCol_inp = s_ma(1,j);     jCol_inp_old = s_ma(1,j-1); 
-                    elseif ii > length(s_ma(1,:)) 
+                    elseif ii > length(s_ma(1,:)) && exist('jCol_inp','var')
                         jCol_inp_old = jCol_inp;  jCol_inp = s_ma(1,j); 
                     elseif ~exist('jCol_inp','var')
-                        jCol_inp = s_ma(1,j);  jCol_inp_old = s_ma(1,j-1);  
+                        jCol_inp = s_ma(1,j);  
+                        if j > 1
+                            jCol_inp_old = s_ma(1,j-1);
+                        else
+                            jCol_inp_old = jCol_inp;
+                        end
                     end
                         jCol_Rt = s_ma(2,j);       jCol_Lt = s_ma(3,j);  
 
