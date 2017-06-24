ffbd63c6712abd563cbb4e3c93b37c1978cd8387
Niklas Kochdumper <niklas.kochdumper@t-online.de>
2017-06-24 16:29:25 +0200

    RESTRUCTURING: add documentation files
    The documentation files got moved from the repository "Toolbox Management" to the repository "sssMOR"

---
+++ doc/source/acknowledgements_sssMOR.m
@@ -0,0 +1,46 @@
+%% Ackowledgements
+% The development of the toolbox as it is could not have been achieved
+% without the support of a whole series of people, first and foremost the
+% students at MORLab that have been working hard in these past few months
+% to bring everything in shape. 
+%
+% In addition, we would like to explicitly
+% acknowledge the work of Rudy Eid, Heiko Panzer and Sylvia Cremer who
+% have laid the foundations for the toolbox and the functions as they are
+% today. 
+%
+% Further, Nico-Philipp Walz and the team of
+% <http://www.itm.uni-stuttgart.de/research/model_reduction/MOREMBS_en.php
+% MOREMBS> in Stuttgart are thanked for their help on creating the MATLAB
+% documentation that comes along with this toolbox.
+%
+% Moreover, we want to thank Jens Saak and Martin Koehler for providing us
+% the <http://www.mpi-magdeburg.mpg.de/projects/mess M.E.S.S.> toolbox. 
+
+%%
+% <html>
+%   <hr>
+%   <p class="copy">&copy; 2016 RT Technische Universit&auml;t M&uuml;nchen
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="https://www.rt.mw.tum.de/?sssMOR">Website</a>
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="file:txts/LICENSE.txt">License</a>
+%        <tt class="minicdot">&#149;</tt>
+%        <a href="file:txts/RELEASE.txt">Release Notes</a>
+%   </p>
+% <div>
+% <table>
+%  <tr>
+%   <td style="background-color:#ffffff; border:0; width:25%; vertical-align:middle; text-align:center">
+%             <img src="img/logo_sssMOR_long.png" alt="sssMOR_Logo" height="40px">
+%      </td>
+%   <td style="background-color:#ffffff; border:0; width:25%; vertical-align:middle; text-align:center">
+%      <img src="img/MORLAB_Logo.jpg" alt="MORLAB_Logo" height="40px"></td>
+%   <td style="background-color:#ffffff; border:0; width:25%; vertical-align:middle; text-align:center">
+%      <img src="img/Logo_Textzusatz_rechts_engl_Chair.png" alt="RT_Logo" height="40px"></td>
+%   <td style="background-color:#ffffff; border:0; width:25%; vertical-align:middle; text-align:center">
+%      <img src="img/TUM-logo.png" alt="TUM_Logo" height="40px"></td>
+%  </tr>
+% </table>
+% </div>
+% </html>
