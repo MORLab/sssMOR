a5960ce8bbd62ff3f85a3ae978308ed9e8b1abe6
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-07-13 14:04:27 +0200

    updated LICENSE and acknowledgements

--- doc/source/acknowledgements_sssMOR.m
+++ doc/source/acknowledgements_sssMOR.m
@@ -1,21 +1,20 @@
 %% Ackowledgements
 % The development of the toolbox as it is could not have been achieved
 % without the support of a whole series of people, first and foremost the
-% students at MORLab that have been working hard in these past few months
-% to bring everything in shape. 
+% *students at MORLab* that have been working hard in the past years
+% to bring everything in shape. A deep acknowledgment to the *German
+% Research Foundation* (DFG) for the funds provided through Grant
+% LO408/19-1.
 %
 % In addition, we would like to explicitly
-% acknowledge the work of Rudy Eid, Heiko Panzer and Sylvia Cremer who
+% acknowledge the work of *Rudy Eid*, *Heiko Panzer* and *Sylvia Cremer* who
 % have laid the foundations for the toolbox and the functions as they are
 % today. 
 %
-% Further, Nico-Philipp Walz and the team of
+% Further, *Nico-Philipp Walz* and the team of
 % <http://www.itm.uni-stuttgart.de/research/model_reduction/MOREMBS_en.php
 % MOREMBS> in Stuttgart are thanked for their help on creating the MATLAB
 % documentation that comes along with this toolbox.
-%
-% Moreover, we want to thank Jens Saak and Martin Koehler for providing us
-% the <http://www.mpi-magdeburg.mpg.de/projects/mess M.E.S.S.> toolbox. 
 
 %%
 % <html>
