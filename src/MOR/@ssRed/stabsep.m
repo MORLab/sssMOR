64d9853f93719b5c55fca14a5f302125b3450868
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-08-02 09:00:00 +0200

    ssRed.stabsep - header and formatting

--- src/MOR/@ssRed/stabsep.m
+++ src/MOR/@ssRed/stabsep.m
@@ -2,113 +2,54 @@ function [sysS, sysAs, Vs, Ws, Vas, Was] = stabsep(sys)
 % STABSEP - Stable-unstable decomposition
 % 
 % Syntax:
-%		output1				= HEADERTEMPLATE(input1)
-%		output1				= HEADERTEMPLATE(input1,input2)
-%		[output1,output2]	= HEADERTEMPLATE(input1,input2,Opts)
+%		sysS                            = STABSEP(sys)
+%		[sysS, sysAs]                   = STABSEP(sys)
+%		[sysS, sysAs, Vs, Ws, Vas, Was]	= STABSEP(sys)
 % 
 % Description:
-%       This header file is to be copied and pasted at the beginning
-%       of every script and function within the sssMORLab toolbox for reference.
-% 
-%       The description should be kept as short as possible but as long as
-%       required to give a good impression of what the function/script is 
-%       about and how it should be used.
-%       
-%       You can use Matlab Markup in normal paragraph text, but not inside 
-%       HTML tables (for example the tables from input/output arguments art
-%       HTML tables)
+%       This function return the decomposition of a dynamic system
+%       (|ssRed| object) into a stable and antistable part such that
+%       |sys = sysS + sysAs|. Eigenvalues on the imaginary axis will be
+%       attributed to the antistable part.
 %
-%       Matlab Markup doesn't 
-%       recognize line breaks.
-%
-%		//Note: the building.mat file for the example can be found in the
-%		benchmarks folder or you can download them from the SLICOT website
-%
-%       It only recognizes a new paragraph when theres is an empty line in
-%       between.
-%
-%       Due to the parsing of the headers by the publishHelp function, the
-%       "preformatted text" and "code" feature of Matlab Markup can't be
-%       used normally. To display code use the ">" as shown below
-%
-%       To display Matlab code use the ">" marker, for example
-%
-%>  x = 2*pi*(0:0.01:1)
-%>  y = sin(x)
-% 
-%       This generates inline |MONOSPACED Text|
-% 
-%       This generates _ITALIC Text_
-% 
-%       This generates *BOLD Text*
-% 
-%       This generates a <https://www.rt.mw.tum.de URL>
-%
-%       This generates inline $LaTeX: x^2+e^{\pi i}$
-% 
-%       To insert a bulleted or numbered list, leave an empty line before
-%       the list:
-% 
-%       * ITEM1
-%       * ITEM2
-% 
-%       # ITEM1
-%       # ITEM2
-%
-%       To insert an image, leave an empty line and then put the filename
-%       between ""<<_>>". Notice that the image can be local or online. If
-%       the image is local, it has to be stored inside of doc/html/img. The
-%       path to the local image is the path between the generated HTML file
-%       and the image.
-%
-%        *Local image:*
-%
-%       <<img/iss1.jpg>>
-%       
-%       *Internet image:*
-% 
-%       <<https://www.rt.mw.tum.de/fileadmin/w00bhf/www/_migrated_pics/Logo_blau_Homepage_01.gif>>
+%       In addition, it returns the projection matrices |Vs|, |Ws| and 
+%       |Vas|, |Was| used to obtain |sysS| and |sysAs| from a projection 
+%       of |sys| (cp. |projectiveMor|).
 %
 % Input Arguments:
 %		*Required Input Arguments:*
-%		-input1: 		Description of in1
-%		-input2: 		Description of in2
-%
-%		*Optional Input Arguments:*
-%		-input3:   		Description of in3
-%						//Note: note inside a table must be on new line
-%		-Opts:	 		Description e.g.: A structure containing following fields
-%			-.field1:  	description of Opts.field1 function;
-% 						[opt1 / opt2 / {default opt}]
-%			-.field2:  	description of Opts.field2 function;
-% 						[opt1 / {opt2} / opt3]
-%			-.field3:  	description of Opts.field3 function;
-% 						[{opt1} / opt2 / opt3]
-%						or line break on a new line
-%						//Note: note inside a table must be on new line
+%		-sys:           ssRed object with original model
 %
 % Output Arguments:
-%       -output1: Description 
-%       -output2: Description
+%       -sysS:      Stable subsystem 
+%       -sysAs:     Antistable subsystem
+%       -Vs,Ws:     projection matrices wrt. stable subsystem
+%       -Vas,Was:   projection matrices wrt. antistable subsystem
 %
 % Examples:
-%		This code loads a benchmark model included in the toolbox
-%		and plots its bode diagram using the sparse state space class:
+%		This code generate a synthetic unstable ssRed object
+%		with eigenvalues left, right, and on the imaginary axis:
 %
-%> load building; 
-%> sys = sss(A,B,C);
-%> bode(sys);
+%> A    = diag([1 0 -1]); b = ones(3,1); c = ones(1,3); 
+%> sys  = ssRed(A,b,c);
+%> disp(sys)
+%> figure; plot(complex(eig(sys)),'bx');hold on
 %
-%//Note: the building.mat file for the example can be found in the benchmarks
-%		folder or you can download them from the SLICOT website
+%       Using |stabsep|, we can separate the asymptotically stable from the
+%       antistable part:
+%
+%> [sysS, sysAs] = stabsep(sys);
+%> disp(sysS), disp(sysAs)
+%
+%       We can compare the eigenvalues of the subsystem to the eigenvalues
+%       of the original model:
+%
+%> plot(complex(eig(sysS)),'go'); plot(complex(eig(sysAs)),'ro');
+%> legend('original','stable','antistable')
 %
 % See Also: 
-%		arnoldi
+%		stabsep, projectiveMor, modalMor
 %
-% References:
-%		* *[1] Grimme (1997)*, Krylov projection methods for model reduction
-%		* *[2] Antoulas (2005)*, Approximation of large-scale dynamical systems
-%		* *[3] Giraud (2005)*, The loss of orthogonality in the Gram-Schmidt...
 %
 %------------------------------------------------------------------
 % This file is part of <a href="matlab:docsearch sssMOR">sssMOR</a>, a Sparse State-Space, Model Order 
@@ -125,7 +66,7 @@ function [sysS, sysAs, Vs, Ws, Vas, Was] = stabsep(sys)
 % Email:        <a href="mailto:sssMOR@rt.mw.tum.de">sssMOR@rt.mw.tum.de</a>
 % Website:      <a href="https://www.rt.mw.tum.de/?sssMOR">www.rt.mw.tum.de/?sssMOR</a>
 % Work Adress:  Technische Universitaet Muenchen
-% Last Change:  01 Aut 2017
+% Last Change:  02 Aug 2017
 % Copyright (c) 2017 Chair of Automatic Control, TU Muenchen
 %------------------------------------------------------------------
 
@@ -138,8 +79,9 @@ function [sysS, sysAs, Vs, Ws, Vas, Was] = stabsep(sys)
     end
     
     %% Find unstable eigenvalues
-    iAs = find(real(diag(D))>0);
-    
+    iAs = find(real(diag(D))>=0);
+       
+    %% Perform decomposition
     if isempty(iAs)
         % System is stable
         sysS    = sys;
@@ -185,7 +127,6 @@ function [sysS, sysAs, Vs, Ws, Vas, Was] = stabsep(sys)
         if isempty(sysAs.(sys.e_)), sysAs.(sys.e_) 	= eye(sysAs.n); end
     end
 end
-
     
 %% ------------------ AUXILIARY FUNCTIONS --------------------------
 function [V, W] = makeReal(V, W)
