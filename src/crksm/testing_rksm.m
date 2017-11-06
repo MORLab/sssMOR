a6dbc8388b8422894e8a2a3ecac7c5823c9762f2
Paul Heidenreich <paul.heidenreich@tum.de>
2017-11-06 17:05:39 +0100

    crksm and adi test script

--- src/crksm/testing_rksm.m
+++ src/crksm/testing_rksm.m
@@ -5,7 +5,7 @@ clearvars -global
 %% testdaten My_rksm
 
 % load any benchmark system
-sys = loadSss('fom');
+sys = loadSss('rail_1357');
 
 % other models: rail_1357, rail_5177, iss, gyro, building, CDplayer, beam,
 %               eady, heat-cont, LF10, random, bips98_606,
@@ -28,97 +28,88 @@ p = size(C,1);
 
 % initialize shifts with irka or function 'initializeShifts'
 % irka
-s0_inp = zeros(1,10);
-Rt = ones(m,size(s0_inp,10));
-Lt = ones(p,size(s0_inp,10));
+s0_inp = zeros(1,20);
+Rt = ones(m,size(s0_inp,20));
+Lt = ones(p,size(s0_inp,20));
 tic
-[sysr_irka, ~, ~, s0_inp, Rt, Lt] = irka(sys,s0_inp,Rt,Lt);
+%[sysr_irka, ~, ~, s0_inp, Rt, Lt] = irka(sys,s0_inp,Rt,Lt);
 time_irka = toc;
 
 Opts.strategy = 'eigs';
-[s0_inp,Rt,s0_out,Lt] = initializeShifts(sys,10,1,Opts);
-%s0_inp = reshape(s0,[1,20]);
-%Opts.orth = 0;
-%[V_arn,Sv_arn,Rv_arn] = arnoldi(sys.E,sys.A,sys.B,s0_inp,Opts);
-%s0_inp = [0 inf 0 inf 0 inf 0 inf 0 inf 0 inf];
-%s0_out = s0;
+[s0_inp1,Rt1,s0_out1,Lt1] = initializeShifts(sys,10,1,Opts);
+%s0_inp1 = initializeShifts(sys,10,1,Opts);
+Opts.strategy = 'ADI';
+[s0_inp2,Rt2,s0_out2,Lt2] = initializeShifts(sys,10,1,Opts);
+%s0_inp2 = initializeShifts(sys,10,1,Opts);
+%s0_inp2 = s0_inp2';
+Opts.strategy = 'ROM';
+
+
 
 
 % call crksm and other functions
-Opts.maxiter_rksm = 40;
-Opts.rctolLyap    =  1e-7; 
-Opts.residual  = 'residual_lyap';  
-%Opts.residual  = 'norm_chol';
+Opts.maxiter = 30;
+Opts.restolLyap   =  1e-6; 
+Opts.stopCrit  = 'residualLyap';  
+%Opts.stopCrit  = 'normChol';
 %Opts.purpose = 'MOR';
 Opts.shifts = 'dynamical';
 %Opts.shifts = 'fixedCyclic';
 Opts.strategy = 'adaptive';
+Opts.adiMethod = 'heur';
 %Opts.rksmnorm = 'fro';
-Opts.stopCrit = 'sysr';
+%Opts.stopCrit = 'sysr';
 %Opts.equation = 'observe';
 
     % call rk
     tic
-    %[sysr_rk,V_rk,W_rk] = rk(sys,s0_inp);
+    %[sysr_rk,Vrk,Wrk] = rk(sys,s0_inp);
     time_rk = toc;
 
     % call function crksm
     tic
-    [sysr,V_crksm,W_crksm,S,R,data_crksm] = crksm(sys,s0_inp,Opts);
+    [sysr,Vcrksm,Wcrksm,S,R,data_crksm] = crksm(sys,s0_inp2,Opts);
     time_crksm=toc;
-      
-    % test for tangential cascaded subspace
-    %tic
-    %[V_tan_cas,W_tan_cas] = test1(sys,s0_inp,Rt,Opts);
-    %time_test1 = toc;
-        
-    alpha1 = subspace(data_out.out4,V_rk);
-    alpha2 = subspace(data_out.out4,Varnoldi);
-    alpha3 = subspace(Varnoldi,V_rk);
-
-
+    
+    % mess-adi options
+    Opts.method = 'adi';
+    Opts.messPara = 'heur';    % only for MESS
+    Opts.rctol = 0;
+    Opts.restol = 1e-7;
+    Opts.norm = 2;
+    Opts.info = 1;
+    
+    tic
+    [Sadi,Radi] = lyapchol(sys,Opts);
+    time_adi = toc;
+    
+    % test subspace
+    %alpha1 = subspace(Vcrksm,Vrk);
 
 %% testing quality of solution (when dimension is high, comment out n>1500)
-% V = data_out.V_basis;
-% %W = data_out.W_basis;
-% if isempty(W)
-%     Pr = S'*S;
-%     P = V*Pr*V';
-%    % P  = Pr'*Pr;
-% else
-%     Pr = W*S*V';
-%     P  = Pr*Pr';
-% end
-%  
-% Y = A*P*E' + E*P*A' + B*B';
-% Y_norm = norm(Y);
 
+Pr = S'*S;
+P = Vcrksm*Pr*Vcrksm';
+% Pr = W*S*V';
+% P  = Pr*Pr';
+YS = A*P*E' + E*P*A' + B*B';
+YS_norm = norm(YS);
 
-% comparing with other hammarling-method
-% Opts.method = 'hammarling';
-% [Shammarling,Rhammarling] = lyapchol(sys,Opts);
-% Phammarling = Shammarling*Shammarling';
-% Yhammarling = A*Phammarling*E' + E*Phammarling*A' + B*B';
-% Yhammarling_norm = norm(Yhammarling);
+Qr = R'*R;
+Q = Vcrksm*Qr*Vcrksm';
+YR = A'*Q*E + E'*Q*A + C'*C;
+YR_norm = norm(YR);
 
 
 
-% mess-adi options
-% Opts.method = 'adi';
-% Opts.messPara = 'heur';    % only for MESS
-% Opts.rctol = 0;
-% Opts.restol = 1e-5;
-% Opts.norm = 2;
-% 
-% call 
-% tic
-% [Sadi,Radi] = lyapchol(sys,Opts);
-% time_adi = toc;
-% 
-% testing quality of solution (when dimension is high, comment out n>1500)
-% Padi = Sadi*Sadi';
-% Yadi = A*Padi*E' + E*Padi*A' + B*B';
-% Yadi_norm = norm(Yadi);
+% ADI solution
+Padi = Sadi*Sadi';
+YSadi = A*Padi*E' + E*Padi*A' + B*B';
+YSadi_norm = norm(YSadi);
+
+Qadi = Radi*Radi';
+YRadi = A'*Qadi*E + E'*Qadi*A + C'*C;
+YRadi_norm = norm(YRadi);
 
 % save data from workspace in your path, comment the following in
 % file_name = 'eady';
